976e46b08fbf8146cede173d700c4219b0e81267
arinsarker07-oss <arinsarker07@gmail.com>
2026-05-22 10:35:07 +0600

    delet added

--- index.js
+++ index.js
@@ -66,7 +66,14 @@ async function run() {
       );
       res.send(result);
     });
-
+    
+    // delete 
+    app.delete('/delete-tutor/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await TutorDetailsCollection.deleteOne(query);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
